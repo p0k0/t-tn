a2c08089639dbd4bb6f2578394d19ee6323cc07c
bmuhametshin <m11e@ya.ru>
2020-06-04 14:34:57 +0500

    поправлены тесты

--- telephone-number/telephone-number/Tree.cs
+++ telephone-number/telephone-number/Tree.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace telephone_number
 {
     public class Tree
     {
+        private IList<IList<char>> _rootTrees;//roots numbers
+
+        public Tree()
+        {
+            _rootTrees = new List<IList<char>>();
+        }
+
+        public void Insert(string v)
+        {
+            if (!_rootTrees.Any())
+                _rootTrees.Add(new List<char>());
+
+            _rootTrees.Where(x => x.FirstOrDefault() != null && x == ).Add(v);
+        }
+
+        public int NodeCount()
+        {
+            return _rootTrees.Count;
+        }
+
+        public IEnumerable<string> Find(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
