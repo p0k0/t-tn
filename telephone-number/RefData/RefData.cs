d86324775c96c8ec522995ea80bb94a98e3fe741
bmuhametshin <m11e@ya.ru>
2020-06-06 16:46:26 +0500

    restructurize + test

--- telephone-number/RefData/RefData.cs
+++ telephone-number/RefData/RefData.cs
@@ -10,7 +10,7 @@ namespace Storage
         public static RefData Null = null;
 
         public RefData Root { get; private set; } = Null;
-        public IEnumerable<RefData> Nodes { get; private set; } = Enumerable.Empty<RefData>();
+        public IEnumerable<RefData> SubNodes { get; private set; } = Enumerable.Empty<RefData>();
 
         public char Data { get; set; }
 
@@ -22,10 +22,8 @@ namespace Storage
         public void AppendSub(RefData subRefData)
         {
             subRefData.Root = this;
-            Nodes = new List<RefData>{ subRefData }.Concat(Nodes);
+            SubNodes = new List<RefData>{ subRefData }.Concat(SubNodes);
         }
-
-        public bool HasParent() => Root != Null;
 
         public override bool Equals(object obj)
         {
@@ -35,115 +33,6 @@ namespace Storage
         public override int GetHashCode()
         {
             return Data.GetHashCode();
-        }
-    }
-
-    public class RefDataScope // compactifyed refData groups, where two group with same lead digits can use same data
-    {
-        //scopeHeads - mean numbers beginnigs
-        private IList<RefData> _heads;
-        private RefDataHeadTraverser _headTraverser;
-        private RefDataFactory _factory;
-
-        public RefDataScope()
-        {
-            _heads = new List<RefData>();
-            _headTraverser = new RefDataHeadTraverser(_heads);
-            _factory = new RefDataFactory();
-        }
-
-        public void Add(string number)
-        {
-            var head = default(RefData);
-            if (!_headTraverser.TryFind(number, out head))
-            {
-                var prefixPath = _factory.Create(number);
-                head = prefixPath.First();
-                _heads.Add(head);
-                return;
-            }
-
-            //TODO:traverse from node until find unequal digit
-        }
-    }
-
-    public class RefDataHeadTraverser//find numbers over group of refData's
-    {
-        private readonly IEnumerable<RefData> _refDatas;
-
-        public RefDataHeadTraverser(IEnumerable<RefData> refDatas)
-        {
-            _refDatas = refDatas ?? throw new ArgumentNullException(nameof(refDatas));
-        }
-
-        public bool TryFind(string prefix, out RefData refData)
-        {
-            var refHead = default(RefData);
-            foreach (var digit in prefix)
-            {
-                refHead = _refDatas.FirstOrDefault(x => x.Data == digit);
-                break;
-            }
-
-            refData = refHead;
-
-            if (refHead != default(RefData))
-                return true;
-
-            return false;
-        }
-    }
-
-    public class RefDataNodeTraverser//find numbers over group of refData's
-    {
-        private readonly RefData _refData;
-
-        public RefDataNodeTraverser(RefData refData)
-        {
-            _refData = refData ?? throw new ArgumentNullException(nameof(refData));
-        }
-
-        public bool TryFind(string prefix, out RefData refData)
-        {
-            //TODO: find/implement traverse from head to tail using parent referencing tree style
-            var refHead = default(RefData);
-            foreach (var digit in prefix)
-            {
-                refHead = _refData.FirstOrDefault(x => x.Data == digit);
-                break;
-            }
-
-            refData = refHead;
-
-            if (refHead != default(RefData))
-                return true;
-
-            return false;
-        }
-    }
-
-    public class RefDataFactory
-    {
-        public RefData Create(char data) => new RefData(data);
-
-        public IEnumerable<RefData> Create(string number)
-        {
-            var result = new List<RefData>();
-            var headData = number.Substring(0, 1).ToCharArray()[0];
-            var head = new RefData(headData);
-            var current = default(RefData);
-
-            result.Add(head);
-            for (int i = 1; i < number.Length; i++)
-            {
-                current = new RefData(number[i]);
-                head.AppendSub(current);
-                result.Add(current);
-
-                head = current;
-            }
-
-            return result;
         }
     }
 }
---
+++ telephone-number/RefData/RefDataFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storage
+{
+    public class RefDataFactory
+    {
+        public RefData Create(char data) => new RefData(data);
+
+        public RefData Create(string number)
+        {
+            var node = default(RefData);
+            foreach (var digit in number.Reverse())
+            {
+                if (node != null)
+                {
+                    node.AppendSub(Create(digit));
+                    node = node.SubNodes.FirstOrDefault();
+                }
+                else
+                {
+                    node = Create(digit);
+                }
+            }
+
+            return node;
+        }
+    }
+}
