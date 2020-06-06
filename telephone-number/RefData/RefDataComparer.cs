5354e17659cc3ac20f281f913dd61221416e08ab
bmuhametshin <m11e@ya.ru>
2020-06-06 17:27:41 +0500

    comparer idea defined

---
+++ telephone-number/RefData/RefDataComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage
+{
+    public class RefDataComparer
+    {
+        /*
+         given:
+         refData A = "12345"
+         refData B = "12397"
+         assume:
+         comparer(A,B) return "***45" or "***97"
+         or 
+         A.Comparer(B) return "***97" //return part of B that different from A
+         B.Comparer(A) return "***45"//return part of A that different from B
+
+         */
+    }
+}
