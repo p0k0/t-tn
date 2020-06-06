08058fb3276cca9da455b8c303b40ac3ca2d7b4c
bmuhametshin <m11e@ya.ru>
2020-06-06 18:36:06 +0500

    refdata accumulative traverser with branchind added

--- telephone-number/telephone-number.tests/TraverseTests.cs
+++ telephone-number/telephone-number.tests/TraverseTests.cs
@@ -16,11 +16,34 @@ namespace telephone_number.tests
             var node = factory.Create("0123456789");
             var target = factory.Create('7');
             var visitor = new FindVisitor(target);
-            var traverser = new RefDataTraverseWithCondition();
+            var traverser = new RefDataDownsideTraverseWithCondition();
 
             var traversResult = traverser.Traverse(node, visitor);
 
             Assert.True(traversResult);
+        }
+
+        [Fact]
+        public void Accumulate_traverse_when_tree_is_line_should_accumulate()
+        {
+            var factory = new RefDataFactory();
+            
+            var nodeA = factory.Create("0123456789");
+            var nodeB = factory.Create("8888");
+
+            nodeA.SubNodes.First()//node 1
+                 .SubNodes.First()//node 2
+                 .SubNodes.First()//node 3
+                 .AppendSub(nodeB);
+
+            var visitor = new AccumulateVisitor();
+            var traverser = new RefDataDownsideAccumulateTraverseWithBranch("0123456789");
+
+            traverser.Traverse(nodeA, visitor);
+
+            var expected = "0123456789";
+
+            Assert.Equal(expected, visitor.GetTraversedPath());
         }
     }
 }
