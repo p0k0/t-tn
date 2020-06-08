7d05ed9ef3c920683300eea842669f4b2d499d4a
bmuhametshin <m11e@ya.ru>
2020-06-08 17:01:05 +0500

    TVector added

---
+++ App.Storage/Storage.v2.Tests/tVector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Storage.v2.Tests
+{
+    public class TVector<T>
+    {
+        private IDictionary<int, T> _data;
+
+        public TVector()
+        {
+            _data = new Dictionary<int, T>();
+        }
+
+        public T this[int i]
+        {
+            get { return _data[i]; }
+            set { _data[i] = value; }
+        }
+    }
+}
