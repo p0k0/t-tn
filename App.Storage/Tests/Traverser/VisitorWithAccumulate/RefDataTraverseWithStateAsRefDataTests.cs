49d9dd0f3ce15e1292e64895d5702a4e6a1e45cc
bmuhametshin <m11e@ya.ru>
2020-06-08 11:04:55 +0500

    test  hierarchy reorganize

---
+++ App.Storage/Tests/Traverser/VisitorWithAccumulate/RefDataTraverseWithStateAsRefDataTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace Storage.Tests.Traverser.VisitorWithAccumulate
+{
+    public class RefDataTraverseWithStateAsRefDataTests
+    {
+        [Fact]
+        public void Accumulate_traverse_with_straight_source_branch_should_accumulate()
+        {
+            var factory = new RefDataFactory();
+
+            var nodeA = factory.Create("0123456789");
+            var nodeB = factory.Create("8888");
+
+            nodeA.SubNodes.First()//node 1
+                 .SubNodes.First()//node 2
+                 .SubNodes.First()//node 3
+                 .AppendSub(nodeB);
+
+            var target = factory.Create("012388");
+            var visitor = factory.CreateAccumulateVisitorWithStateAsString();
+            var traverser = new RefDataDownsideAccumulateTraverseWithStraightBranch(target);
+
+            traverser.Traverse(nodeA, visitor);
+
+            var expected = "012388";
+
+            Assert.Equal(expected, visitor.GetState().ToString());
+        }
+    }
+}
---
+++ App.Storage/Tests/Traverser/VisitorWithAccumulate/RefDataTraverseWithStateAsStringTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace Storage.Tests.Traverser.VisitorWithAccumulate
+{
+    public class RefDataTraverseWithStateAsStringTests
+    {
+        [Fact]
+        public void Accumulate_traverse_when_tree_is_line_should_accumulate()
+        {
+            var factory = new RefDataFactory();
+
+            var nodeA = factory.Create("0123456789");
+            var nodeB = factory.Create("8888");
+
+            nodeA.SubNodes.First()//node 1
+                 .SubNodes.First()//node 2
+                 .SubNodes.First()//node 3
+                 .AppendSub(nodeB);
+
+            var visitor = factory.CreateAccumulateVisitorWithStateAsString();
+            var traverser = new RefDataDownsideAccumulateTraverseWithBranchPathAsString("0123456789");
+
+            traverser.Traverse(nodeA, visitor);
+
+            var expected = "0123456789";
+
+            Assert.Equal(expected, visitor.GetState().ToString());
+        }
+    }
+}
