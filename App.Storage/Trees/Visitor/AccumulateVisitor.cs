a301ebb0b779114978f270505d57bd5c5f4e6202
bmuhametshin <m11e@ya.ru>
2020-06-10 14:04:02 +0500

    AccumulateVisitor - переопределен Equals, ToString, добавлен constructor

--- App.Storage/Trees/Visitor/AccumulateVisitor.cs
+++ App.Storage/Trees/Visitor/AccumulateVisitor.cs
@@ -13,10 +13,35 @@ namespace Trees.Visitor
             TraversedPath = new StringBuilder();
         }
 
+        public AccumulateVisitor(string traversedPath)
+        {
+            TraversedPath = new StringBuilder(traversedPath);
+        }
+
+        public AccumulateVisitor(AccumulateVisitor another)
+        {
+            TraversedPath = new StringBuilder(another.ToString());
+        }
+
         public void Visit(Node node)
         {
             node.HasVisited = true;
             TraversedPath.Append(node.Data);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ToString().Equals( ((AccumulateVisitor)obj).ToString() );
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return TraversedPath.ToString();
         }
     }
 }
