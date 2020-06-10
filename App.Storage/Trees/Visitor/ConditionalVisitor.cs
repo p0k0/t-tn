e8cd1c566c527a3d4e2e567b5377b911f1c64be3
bmuhametshin <m11e@ya.ru>
2020-06-10 09:37:45 +0500

    Добален *Visitor

---
+++ App.Storage/Trees/Visitor/ConditionalVisitor.cs
@@ -0,0 +1,7 @@
+﻿namespace Trees.Visitor
+{
+    public abstract class ConditionalVisitor
+    {
+        public abstract bool Visit(Node node);
+    }
+}
---
+++ App.Storage/Trees/Visitor/FindNodeVisitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Trees.Visitor
+{
+    public class FindNodeVisitor : ConditionalVisitor
+    {
+        private readonly Node _targetNode;
+
+        public FindNodeVisitor(Node targetNode)
+        {
+            _targetNode = targetNode ?? throw new ArgumentNullException(nameof(targetNode));
+        }
+
+        public override bool Visit(Node node)
+        {
+            if (node.Data != _targetNode.Data)
+                return false;
+
+            return true;
+        }
+    }
+}
