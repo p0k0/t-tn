deb0be7b26e59be6b978a495cc85d8c4c95fa6e6
bmuhametshin <m11e@ya.ru>
2020-06-10 10:04:52 +0500

    Добавлен DeepFirstSearch Iterator с предикатом

---
+++ App.Storage/Trees.Tests/Iterator/DeepFirstSearchIteratorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Trees.Tests.Iterator
+{
+    public class DeepFirstSearchIteratorTests
+    {
+        [Fact]
+        public void Should_find_node_when_it_possible()
+        {
+            var factory = new TreeFactory();
+            var iterator = new DeepFirstSearchIterator();
+
+            var treeHead = factory.CreateStraightTree("01234");
+            var searchResult = iterator.Find(treeHead, node => node.Data == '3');
+            var expected = new Node { Data = '3' };
+
+            Assert.Equal(expected, searchResult);
+        }
+    }
+}
---
+++ App.Storage/Trees/Iterator/DeepFirstSearchIterator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Trees
+{
+    public class DeepFirstSearchIterator
+    {
+        public Node Find(Node startNode, Expression<Func<Node, bool>> predicate)
+        {
+            if (predicate.Compile().Invoke(startNode))
+                return startNode;
+
+            foreach (var sub in startNode.SubNodes)
+                return Find(startNode: sub, predicate);
+
+            return null;
+        }
+    }
+}
