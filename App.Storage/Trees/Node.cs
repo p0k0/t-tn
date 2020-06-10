c7bf41c9e66d50845be067d243ac0458a19a2f9e
bmuhametshin <m11e@ya.ru>
2020-06-10 08:46:17 +0500

    Добавлена базовая сущность для вершин дерева

---
+++ App.Storage/Trees/Node.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trees
+{
+    public class Node
+    {
+        public Node Parent { get; protected set; }
+        public ICollection<Node> SubNodes { get; protected set; }
+    }
+}
