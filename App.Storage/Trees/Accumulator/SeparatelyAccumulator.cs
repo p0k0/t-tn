651d97785a8e4a00f3940211b7cf962e3ec52be2
bmuhametshin <m11e@ya.ru>
2020-06-10 14:02:26 +0500

    Добавлен SeparatelyAccumulator

---
+++ App.Storage/Trees/Accumulator/SeparatelyAccumulator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Trees.Visitor;
+
+namespace Trees.Accumulator
+{
+    public class SeparatelyAccumulator
+    {
+        public IEnumerable<AccumulateVisitor> Accumulate(Node startNode, Expression<Func<Node, bool>> predicate, AccumulateVisitor accumulateVisitor)
+        {
+            accumulateVisitor.Visit(startNode);
+
+            if (predicate.Compile().Invoke(startNode))
+                yield return accumulateVisitor;
+
+            if (startNode.SubNodes.Count == 0)
+                yield return accumulateVisitor;
+
+            if (startNode.SubNodes.Count == 1)
+                yield return Accumulate(startNode.SubNodes.Single(), predicate, accumulateVisitor).Single();
+
+            if (startNode.SubNodes.Count > 1)
+            {
+                foreach (var sub in startNode.SubNodes)
+                    yield return Accumulate(sub, predicate, new AccumulateVisitor(accumulateVisitor)).Single();
+            }
+        }
+    }
+}
