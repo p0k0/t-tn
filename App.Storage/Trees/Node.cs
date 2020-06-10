ff37264dc10c0f0cab02d8d1b9031eca9c8691b9
bmuhametshin <m11e@ya.ru>
2020-06-10 09:06:38 +0500

    Добавлены методы Equals, AppendSub; SubNodes определен как IList<>

--- App.Storage/Trees/Node.cs
+++ App.Storage/Trees/Node.cs
@@ -1,11 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Trees
 {
     public class Node
     {
         public Node Parent { get; protected set; }
-        public ICollection<Node> SubNodes { get; protected set; }
+        public IList<Node> SubNodes { get; protected set; }
+        public char Data { get; set; }
+
+        public Node()
+        {
+            SubNodes = new List<Node>();
+        }
+
+        public void AppendSub(Node newSubNode)
+        {
+            newSubNode.Parent = this;
+            SubNodes.Add(newSubNode);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Data.Equals(((Node)obj).Data);
+        }
+
+        public override int GetHashCode()
+        {
+            return Data.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
     }
 }
