23355141605d08efcb5fba7d74f45719818d76df
bmuhametshin <m11e@ya.ru>
2020-06-10 15:42:44 +0500

    Добавлен тест Storage.Box

--- App.Storage/Storage.Tests/BoxTests.cs
+++ App.Storage/Storage.Tests/BoxTests.cs
@@ -7,7 +7,7 @@ namespace Storage.Tests
     public class BoxTests
     {
         [Fact]
-        public void Should_find_added_node()
+        public void Should_find_pattern_when_exists_one_pattern()
         {
             var box = new Box();
             var pattern = "0467123456";
@@ -17,6 +17,26 @@ namespace Storage.Tests
             var expected = "0467123456";
 
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Should_find_patterns_when_exists_more_one_pattern()
+        {
+            var box = new Box();
+            var patternA = "0123456789";
+            var patternB = "1123456789";
+            
+            box.Add(patternA);
+            box.Add(patternB);
+
+            var foundResultByPatternA = box.Find(patternA).Single();
+            var foundResultByPatternB = box.Find(patternB).Single();
+            
+            var expectedResultA = "0123456789";
+            var expectedResultB = "1123456789";
+
+            Assert.Equal(expectedResultA, foundResultByPatternA);
+            Assert.Equal(expectedResultB, foundResultByPatternB);
         }
     }
 }
