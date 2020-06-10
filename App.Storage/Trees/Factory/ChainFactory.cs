db44bbdb53447897df686f26a21d69e672d0fe0d
bmuhametshin <m11e@ya.ru>
2020-06-10 19:41:42 +0500

    Добавлены ChainFactory, TreeFactory

---
+++ App.Storage/Trees/Factory/ChainFactory.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Trees.Node;
+
+namespace Trees.Factory
+{
+    public class ChainFactory
+    {
+        public ChainNode DeepCopy(INode node)
+        {
+            if (node == null)
+                return null;
+
+            var newNode = new ChainNode
+            {
+                Data = node.Data,
+                HasVisited = node.HasVisited
+            };
+
+            newNode.AppendSub(DeepCopy(node.SubNodes.SingleOrDefault()));
+
+            return newNode;
+        }
+
+        public ChainNode ShallowCopy(INode node)
+        {
+            if (node == null)
+                return null;
+
+            var newNode = new ChainNode
+            {
+                Data = node.Data,
+                HasVisited = node.HasVisited
+            };
+
+            return newNode;
+        }
+
+        public ChainNode Create(string number)
+        {
+            var head = default(ChainNode);
+            var node = default(ChainNode);
+            var newNode = default(ChainNode);
+
+            head = node = new ChainNode { Data = number[0] };
+
+            foreach (var digit in number.Skip(1))
+            {
+                newNode = new ChainNode { Data = digit };
+                node.AppendSub(newNode);
+                node = node.SubNodes[0] as ChainNode;
+            }
+
+            return head as ChainNode;
+        }
+    }
+}
---
+++ App.Storage/Trees/Factory/TreeFactory.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Trees.Node;
+
+namespace Trees.Factory
+{
+    public class TreeFactory
+    {
+        public TreeNode DeepCopy(INode node)
+        {
+            var newNode = new TreeNode
+            {
+                Data = node.Data,
+                HasVisited = node.HasVisited
+            };
+
+            foreach (var sub in node.SubNodes)
+                newNode.AppendSub(DeepCopy(sub));
+
+            return newNode;
+        }
+
+        public TreeNode ShallowCopy(INode node)
+        {
+            if (node == null)
+                return null;
+
+            var newNode = new TreeNode
+            {
+                Data = node.Data,
+                HasVisited = node.HasVisited
+            };
+
+            return newNode;
+        }
+    }
+}
