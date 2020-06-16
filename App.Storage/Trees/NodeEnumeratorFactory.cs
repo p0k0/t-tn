79017a71780537655f60a2a1ac2d4f24cae20705
bmuhametshin <m11e@ya.ru>
2020-06-16 17:44:16 +0500

    Добавлена фабрика для итераторов 2ух типов (пока без реализации)

---
+++ App.Storage/Trees/NodeEnumeratorFactory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public enum NodeTraverseType
+    {
+        InDeepByConcretePath,
+        InDeepByAllSubnodes
+    }
+
+    public class NodeEnumeratorFactory
+    {
+        private readonly Node _node;
+
+        public NodeEnumeratorFactory(Node node)
+        {
+            _node = node;
+        }
+
+        public NodeEnumerator Create(NodeTraverseType type)
+        {
+            switch (type)
+            {
+                case NodeTraverseType.InDeepByConcretePath:
+                    return new NodeInDeepByConcretePathEnumerator(_node);
+                case NodeTraverseType.InDeepByAllSubnodes:
+                    return new NodeInDeepByAllSubnodesEnumerator(_node);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+
+    public abstract class NodeEnumerator : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public virtual bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class NodeInDeepByConcretePathEnumerator : NodeEnumerator
+    {
+        private readonly Node _node;
+
+        public NodeInDeepByConcretePathEnumerator(Node node)
+        {
+            _node = node;
+        }
+
+        public override bool MoveNext()
+        {
+            return base.MoveNext();
+        }
+    }
+
+    public class NodeInDeepByAllSubnodesEnumerator : NodeEnumerator
+    {
+        private readonly Node _node;
+
+        public NodeInDeepByAllSubnodesEnumerator(Node node)
+        {
+            _node = node;
+        }
+
+        public override bool MoveNext()
+        {
+            return base.MoveNext();
+        }
+    }
+}
