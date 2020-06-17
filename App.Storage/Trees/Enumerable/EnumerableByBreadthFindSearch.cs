f323946aa876c272abc03e45b51a1e0a89990203
bmuhametshin <m11e@ya.ru>
2020-06-17 08:14:30 +0500

    Добавлен IEnumerator BFS (Breadth Find Search)

---
+++ App.Storage/Trees/Enumerable/EnumerableByBreadthFindSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Trees.Enumerator;
+
+namespace Trees.Enumerable
+{
+    public class EnumerableByBreadthFindSearch : IEnumerable, IEnumerable<Node>
+    {
+        private readonly Node _startNode;
+
+        public EnumerableByBreadthFindSearch(Node startNode)
+        {
+            _startNode = startNode;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public BreadthFindSearchEnumerator GetEnumerator() => new BreadthFindSearchEnumerator(_startNode);
+
+        IEnumerator<Node> IEnumerable<Node>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ App.Storage/Trees/Enumerator/BreadthFindSearchEnumerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Trees.Enumerator
+{
+    public class BreadthFindSearchEnumerator : NodeEnumerator
+    {
+        private Queue<Node> _firingNode;
+        private Node _currentNode;
+
+        internal BreadthFindSearchEnumerator(Node node)
+        {
+            _firingNode = new Queue<Node>();
+            _firingNode.Enqueue(node);
+            _startNode = node;
+        }
+
+        public override object Current => _currentNode;
+
+        public override bool MoveNext()
+        {
+            if (_firingNode.Count == 0)
+                return false;
+
+            _currentNode = _firingNode.Peek();
+
+            if (_currentNode == null)
+                return false;
+
+            var removed = _firingNode.Dequeue();
+            foreach (var sub in removed.SubNodes)
+                _firingNode.Enqueue(sub);
+
+            return true;
+        }
+    }
+}
